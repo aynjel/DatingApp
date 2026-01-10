6cbf8c40efff2a816b7eba4f546b06a26529b5ce
Anggi <ortegacanillo76.1@gmail.com>
2026-01-10 14:53:19 +0800

    remove checking for member details setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,57 +1,19 @@
-import { Component, computed, effect, inject, Signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from '@component/toast/toast.component';
 import { ModalComponent } from './shared/components/modal/modal.component';
 import { NavComponent } from './shared/components/nav/nav.component';
-import { AuthStore } from './shared/store/auth.store';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavComponent, ToastComponent, ModalComponent],
   template: `
-    <!-- @if(authStore.isLoggedIn() && isMemberDetailsSetup()) { -->
     <app-nav />
-    <!-- } -->
     <div id="main-content" class="mt-[4rem] w-full transition-all duration-300">
-      <!-- [ngClass]="{
-        'mt-[4rem]': authStore.isLoggedIn() && isMemberDetailsSetup()
-      }" -->
       <router-outlet />
     </div>
     <app-toast />
     <app-modal />
   `,
 })
-export class AppComponent {
-  private authStore = inject(AuthStore);
-  private router = inject(Router);
-
-  protected isLoggedIn: Signal<boolean> = computed(() => {
-    return this.authStore.isLoggedIn();
-  });
-
-  protected isMemberDetailsSetup: Signal<boolean> = computed(() => {
-    const memberDetails = this.authStore.memberDetails();
-    if (!memberDetails) return false;
-    return !!memberDetails;
-  });
-
-  private _checkMemberDetails = effect(() => {
-    const currentUser = this.authStore.currentUser();
-    const memberDetails = this.authStore.memberDetails();
-    const currentUrl = this.router.url;
-
-    // If member details are not set up, redirect to setup page
-    // But skip if already on the member-details page to avoid redirect loops
-    if (
-      currentUser &&
-      !memberDetails &&
-      !currentUrl.includes('/profile/member-details')
-    ) {
-      const userId = currentUser.userId;
-      if (userId) {
-        this.router.navigate(['/profile/member-details', userId]);
-      }
-    }
-  });
-}
+export class AppComponent {}
